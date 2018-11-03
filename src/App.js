46461994385243eda37caa39bdfcd38412a59cdf
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-11-03 16:21:42 -0400

    Added header and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="container">
+        <h1>Cafe Moon Light</h1>
+        <div classsname="bg-wrapper" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
